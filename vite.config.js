b39be95f11fa7067a5907f1bc4e2e91d7b930751
NdikumNdifor <sebastienndifor@gmail.com>
2024-12-05 23:21:59 +0100

    More dependency configuration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-import { resolve } from "path";
+import { resolve, dirname } from "path";
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+
+// Define __dirname for ES Modules
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   root: "src/",
