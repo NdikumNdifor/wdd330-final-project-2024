afa5904b2e4ccbe41ad2349296627f2e83c57de3
NdikumNdifor <sebastienndifor@gmail.com>
2024-12-08 17:32:23 +0100

    Added product page routing.

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        product: resolve(__dirname, "src/product-page/index.html"),
       },
     },
   },
