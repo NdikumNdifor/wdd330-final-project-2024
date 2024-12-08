7c7288ef20854e0888a5be6d34972bd1552979c5
NdikumNdifor <sebastienndifor@gmail.com>
2024-12-08 17:48:28 +0100

    Modified data source instantiation

--- src/js/main.js
+++ src/js/main.js
@@ -4,23 +4,22 @@ import { ProductListing } from "./ProductList.mjs";
 
 // Creating an instance of external service class to
 // call the get movies method for movie data
-const externalServices = new ExternalServices();
-const dataSource = externalServices.getMovies();
+const dataSource = new ExternalServices();
+// const dataSource = externalServices.getMovies();
 
 // Grabs an element on the DOM
-const listElement = getElement(".product-listing")
+const listElement = getElement(".product-listing");
 
 // Instantiate the product list class to render the product
 // in the defined template
-const products = new ProductListing(dataSource, listElement)
+const products = new ProductListing(dataSource, listElement);
 
-async function main(){
-    // Loads the header and footer
-    await loadHeaderFooter()
-    
-    // Use object to call the init method that renders template
-    await products.init();
+async function main() {
+  // Loads the header and footer
+  await loadHeaderFooter();
 
+  // Use object to call the init method that renders template
+  await products.init();
 }
 
 // calls the function called main
