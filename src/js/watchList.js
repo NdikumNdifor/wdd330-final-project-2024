d03b0861b1ea1b394e9ba48ce1c77e9a16fb5b45
NdikumNdifor <sebastienndifor@gmail.com>
2024-12-10 23:46:04 +0100

    deleted commented code.

--- src/js/watchList.js
+++ src/js/watchList.js
@@ -16,24 +16,3 @@ const main = async () => {
 
 main()
 
-
-
-// Gets an an object or an empty array where product would be pushed into
-// and push the key value pair object into.
-// function addProductToCart(product) {
-//   const currentCart = getLocalStorage("so-cart") || [];
-//   currentCart.push(product);
-//   setLocalStorage("so-cart", currentCart);
-// }
-// add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await dataSource.findProductById(e.target.dataset.id);
-//   addToCart(product);
-// }
-
-// add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
-
-
